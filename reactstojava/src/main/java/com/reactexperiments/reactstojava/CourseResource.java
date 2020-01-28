f7626b2436e78a038c5ffa23cfc1e711e35b062a
Ioan Siran <ioansiran17@gmail.com>
2020-01-28 23:44:16 +0200

    fixed build path for java app

--- reactstojava/src/main/java/com/reactexperiments/reactstojava/CourseResource.java
+++ reactstojava/src/main/java/com/reactexperiments/reactstojava/CourseResource.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class CourseResource {
 	@Autowired
---
+++ reactstojava/src/main/resources/static/precache-manifest.a90f68374e1caa9dae1b9225535c344b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a5c39af0a426079a93f40cee3f5d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf76551066d4b418dba",
+    "url": "/static/css/main.0c0f25d3.chunk.css"
+  },
+  {
+    "revision": "32ec9dc13b8137a3bc58",
+    "url": "/static/js/2.7d9e0474.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.7d9e0474.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebf76551066d4b418dba",
+    "url": "/static/js/main.504e0876.chunk.js"
+  },
+  {
+    "revision": "aee82e0e4f241b938031",
+    "url": "/static/js/runtime-main.607caeb2.js"
+  }
+]);
